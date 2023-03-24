8a4540702093e8f8811c9efc7e20ca5bada4cc78
minnyres <liuthgw23@gmail.com>
2023-03-24 20:53:56 +0800

    change url to avoid the DNS resolve problem

--- autoconf.sh
+++ autoconf.sh
@@ -3,7 +3,7 @@
 set -e
 
 autoconf_version="2.69"
-wget http://ftpmirror.gnu.org/autoconf/autoconf-${autoconf_version}.tar.xz
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-${autoconf_version}.tar.xz
 tar -xf autoconf-${autoconf_version}.tar.xz
 cd autoconf-${autoconf_version}
 
