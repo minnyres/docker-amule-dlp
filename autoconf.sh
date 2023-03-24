394f45297fe940344275fb4b08c996dad3c09edd
minnyres <liuthgw23@gmail.com>
2023-03-24 21:50:45 +0800

    specify build and host when configure

--- autoconf.sh
+++ autoconf.sh
@@ -7,7 +7,7 @@ wget https://mirrors.sarata.com/gnu/autoconf/autoconf-${autoconf_version}.tar.xz
 tar -xf autoconf-${autoconf_version}.tar.xz
 cd autoconf-${autoconf_version}
 
-./configure --prefix=/usr
+./configure --prefix=/usr --build=$(gcc -dumpmachine) --host=$(gcc -dumpmachine)
 make -j$(nproc)
 make install 
 make clean
