cf671de63e271e755466a64c917e03996fc46e77
minnyres <liuthgw23@gmail.com>
2023-03-24 22:35:03 +0800

    replace all config.guess and config.sub in wxwidgets dir

--- wxbase.sh
+++ wxbase.sh
@@ -7,7 +7,8 @@ wxbase_version="3.0.5"
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v${wxbase_version}/wxWidgets-${wxbase_version}.tar.bz2
 tar -xf wxWidgets-${wxbase_version}.tar.bz2
 cd wxWidgets-${wxbase_version}
-cp /amule-build/config.* .
+find . -name config.guess -exec cp /amule-build/config.guess {} \;
+find . -name config.sub -exec cp /amule-build/config.sub {} \;
 
 ./configure --prefix=/usr --with-zlib=sys  \
     --enable-shared --disable-gui --disable-debug_flag --enable-optimise --enable-unicode --enable-monolithic
